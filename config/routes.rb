1746ee9d4fcec44e595c9117c6a7b615237da06a
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-09-05 02:51:07 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: [:index, :new, :create]
+  resources :students, only: %i[index new create]
   get '/student/:id', to: 'students#show', as: 'student'
 end
